16a5dc782336168dc9c78459b4c988054b6ce0a6
Ghazal <ghazal.kazemi@gmail.com>
2021-06-18 14:32:10 +1000

    Commit Student detail using session object

--- src/com/spring/demo/StudentDemo.java
+++ src/com/spring/demo/StudentDemo.java
@@ -1,2 +1,27 @@
-package com.spring.demo;public class StudentDemo {
+package com.spring.demo;
+
+import com.spring.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class StudentDemo {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            Student tempStudent = new Student("John", "Doe", "john@mail.com");
+            session.beginTransaction();
+            session.save(tempStudent);
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+
+    }
 }
