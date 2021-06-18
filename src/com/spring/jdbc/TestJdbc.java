2efa8dd791b8d8a0f2ca1f8cf70d04e74bc5847e
Ghazal <ghazal.kazemi@gmail.com>
2021-06-18 12:50:35 +1000

    Test jdbc connection to database

---
+++ src/com/spring/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.spring.jdbc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker";
+        try {
+            properties.load(new FileInputStream("data.properties"));
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+            System.out.println("Connection was successful!");
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
