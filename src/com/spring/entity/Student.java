e87e1d132b206f7ca4710487471c9133a2ee4aeb
Ghazal <ghazal.kazemi@gmail.com>
2021-06-18 14:31:06 +1000

    Fix hibernate annotation on Student class

--- src/com/spring/entity/Student.java
+++ src/com/spring/entity/Student.java
@@ -1,20 +1,25 @@
 package com.spring.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "student")
 public class Student {
     @Id
+    @Column(name = "id")
     private int id;
-
+    @Column(name = "first_name")
     private String firstName;
-
+    @Column(name = "last_name")
     private String lastName;
-
+    @Column(name = "email")
     private String email;
 
-    public Student(){}
+    public Student() {
+    }
 
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
