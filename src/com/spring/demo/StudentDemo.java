48e9c84d9ca620b02a62bcfab866146f088be2e7
Ghazal <ghazal.kazemi@gmail.com>
2021-06-18 15:58:11 +1000

    Add more entries to the database

--- src/com/spring/demo/StudentDemo.java
+++ src/com/spring/demo/StudentDemo.java
@@ -15,16 +15,21 @@ public class StudentDemo {
 
         Session session = sessionFactory.getCurrentSession();
         try {
-            Student tempStudent = new Student("John", "Doe", "john@mail.com");
+            //Student tempStudent = new Student("John", "Doe", "john@mail.com");
+            Student mary = new Student("Mary", "Moore", "mary@mail.com");
+            Student kate = new Student("Kate", "Malik", "kate@mail.com");
+            Student jeff = new Student("Jeff", "Match", "jeff@mail.com");
             session.beginTransaction();
-            session.save(tempStudent);
+            session.save(mary);
+            session.save(kate);
+            session.save(jeff);
             session.getTransaction().commit();
 
             // read data
             session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Student student = session.get(Student.class, tempStudent.getId());
+            Student student = session.get(Student.class, mary.getId());
             System.out.println("Read from the database: " + student);
             session.getTransaction().commit();
 
