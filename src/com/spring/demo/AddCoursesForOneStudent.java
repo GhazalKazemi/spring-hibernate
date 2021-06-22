cc5688e118e9dbdf960389a3cc8356b967934626
Ghazal <ghazal.kazemi@gmail.com>
2021-06-22 17:30:37 +1000

    Get student courses

---
+++ src/com/spring/demo/AddCoursesForOneStudent.java
@@ -0,0 +1,38 @@
+package com.spring.demo;
+
+import com.spring.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AddCoursesForOneStudent {
+
+    public static void main(String[] args) {
+
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            int studentId = 2;
+            Student student = session.get(Student.class, studentId);
+            System.out.println("Student courses: " + student.getCourses());
+
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sessionFactory.close();
+            session.close();
+        }
+
+    }
+}
