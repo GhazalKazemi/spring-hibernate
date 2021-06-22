d553d87d6ffb66d80ac5a7d19a708e0f9f86dc53
Ghazal <ghazal.kazemi@gmail.com>
2021-06-22 16:32:44 +1000

    Create association from student to course

--- src/com/spring/entity/Student.java
+++ src/com/spring/entity/Student.java
@@ -1,6 +1,7 @@
 package com.spring.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "student")
@@ -15,6 +16,13 @@ public class Student {
     private String lastName;
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "course_student",
+    joinColumns = @JoinColumn(name = "student_id"),
+    inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<Course> courses;
 
     public Student() {
     }
@@ -55,6 +63,14 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     @Override
